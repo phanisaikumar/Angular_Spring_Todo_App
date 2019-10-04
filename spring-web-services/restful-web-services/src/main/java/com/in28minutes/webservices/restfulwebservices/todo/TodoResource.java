d463635fc390cb49ac5f0d3d386507b397015df1
Phani Sai Kumar Narikalapa -X (pnarikal - ITALENT APPLICATIONS PRIVATE LIMITED at Cisco) <pnarikal@partnet.cisco.com>
2019-10-04 10:08:03 +0530

    added create and update functionalities in backend

--- spring-web-services/restful-web-services/src/main/java/com/in28minutes/webservices/restfulwebservices/todo/TodoResource.java
+++ spring-web-services/restful-web-services/src/main/java/com/in28minutes/webservices/restfulwebservices/todo/TodoResource.java
@@ -1,14 +1,20 @@
 package com.in28minutes.webservices.restfulwebservices.todo;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @CrossOrigin(origins="http://localhost:4200")
 @RestController
@@ -34,6 +40,19 @@ public class TodoResource {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
+    }
+    
+    @PutMapping("users/{username}/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id, @RequestBody Todo todo){
+    	Todo updatedTodo = todoService.Save(todo);
+    	return new ResponseEntity<Todo>(updatedTodo,HttpStatus.OK);
+    }
+    
+    @PostMapping("users/{username}/todos")
+    public ResponseEntity<Void> createTodo(@PathVariable String username, @RequestBody Todo todo){
+    	Todo createdTodo = todoService.Save(todo);
+    	URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdTodo.getId()).toUri();
+    	return ResponseEntity.created(uri).build();
     }
     
 }
